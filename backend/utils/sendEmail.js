0095418a33867430c4d970831ece82035bcf50eb
Diksha Gangolia <dikshagangolia@gmail.com>
2026-07-14 17:34:22 +0530

    Add email debug logs

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,6 +1,9 @@
 const nodemailer = require("nodemailer");
 
 const sendEmail = async (to, productName) => {
+  console.log("EMAIL USER:", process.env.EMAIL_USER);
+  console.log("EMAIL PASS:", process.env.EMAIL_PASS ? "Loaded" : "Missing");
+
   const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
@@ -15,6 +18,8 @@ const sendEmail = async (to, productName) => {
     subject: "BRIXO Order Confirmation",
     text: `Your order for ${productName} has been received successfully. Thank you for shopping with BRIXO!`,
   });
+
+  console.log("EMAIL SENT SUCCESSFULLY");
 };
 
 module.exports = sendEmail;
