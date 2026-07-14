a033c96beb1db241e559880382fd5a4f8d0bd346
Diksha Gangolia <dikshagangolia@gmail.com>
2026-07-14 17:41:26 +0530

    Add email error logs

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,25 +1,36 @@
 const nodemailer = require("nodemailer");
 
 const sendEmail = async (to, productName) => {
+  try {
+    console.log("EMAIL USER:", process.env.EMAIL_USER);
+    console.log(
+      "EMAIL PASS:",
+      process.env.EMAIL_PASS ? "Loaded" : "Missing"
+    );
 
-  const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+    const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
 
-  await transporter.sendMail({
-    from: process.env.EMAIL_USER,
-    to,
-    subject: "BRIXO Order Confirmation",
-    text: `Your order for ${productName} has been received successfully. Thank you for shopping with BRIXO!`,
-  });
+    await transporter.sendMail({
+      from: process.env.EMAIL_USER,
+      to,
+      subject: "BRIXO Order Confirmation",
+      text: `Your order for ${productName} has been received successfully. Thank you for shopping with BRIXO!`,
+    });
 
-  console.log("EMAIL SENT SUCCESSFULLY");
+    console.log("EMAIL SENT SUCCESSFULLY");
+
+  } catch (error) {
+    console.log("EMAIL ERROR:", error.message);
+    throw error;
+  }
 };
 
 module.exports = sendEmail;
